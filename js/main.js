f51f724de5cea2a96cd99d22d352a9702e6c8a60
malakmilad <malakmilad202@gmail.com>
2022-06-26 17:57:42 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*setTimeout(function(){
+    $(".down").fadeOut(2000);   
+},2000);*/
+
+$(".project").ready(function(){
+    $(".down").fadeOut(4000);   
+},4000);
+
+
+
+
+document.getElementById("btn").onclick=function(){
+    $(".contact .item").hide(1000);
+}
